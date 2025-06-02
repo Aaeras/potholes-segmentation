cb79165f9d4bb3402b2ebe0281f9d48eddfcc3ce
aaeras <mainaaeras@gmail.com>
2025-06-02 22:26:24 +1000

    formatting

--- pages/2_🎥_App.py
+++ pages/2_🎥_App.py
@@ -22,21 +22,15 @@ st.title("Automated Pothole Detection")
 if not st.session_state.get("authenticated", False):
     st.warning("You must login first. Please use the Login page.")
     st.stop()
-# Allow user to upload image or video
 uploaded_file = st.file_uploader("Upload an image or video", type=["jpg", "jpeg", "png", "mp4", "mov", "avi", "mkv"])
-
 confidence_threshold = st.slider("Confidence Threshold", 0.0, 1.0, 0.5, 0.01)
 
 if uploaded_file is not None:
     filename = uploaded_file.name.lower()
-
-    # If the file is an image
     if filename.endswith(("jpg", "jpeg", "png")):
-        # Convert uploaded file to a NumPy array for OpenCV
         file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-        img_cv = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)  # BGR image
+        img_cv = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)  
         
-        # Convert to RGB for display
         original_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
         st.image(original_rgb, caption="Original Image", use_container_width=True)
         
@@ -44,11 +38,8 @@ if uploaded_file is not None:
             results = model.predict(source=img_cv, conf=confidence_threshold)
             res = results[0]
             
-            # Get annotated image (res.plot() returns a BGR image)
             annotated_img = res.plot()
-            annotated_rgb = annotated_img[..., ::-1]  # convert BGR to RGB
-            
-            # Display images side by side using Streamlit columns
+            annotated_rgb = annotated_img[..., ::-1] 
             col1, col2 = st.columns(2)
             with col1:
                 st.image(original_rgb, caption="Original Image", use_container_width=True)
@@ -75,7 +66,7 @@ if uploaded_file is not None:
                     break
 
                 results = model.predict(source=frame, conf=confidence_threshold)
-                result = results[0]  # YOLOv11 returns list-like results
+                result = results[0] 
 
                 # Draw and display
                 annotated_frame = result.plot()
@@ -83,7 +74,6 @@ if uploaded_file is not None:
                 stframe.image(annotated_frame_rgb, channels="RGB", use_container_width=True)
                 #time.sleep(speed / 1000.0)
 
-                # Collect detections
                 boxes = result.boxes
                 if boxes is not None and hasattr(boxes, 'cls'):
                     for cls_id, conf, box_coords in zip(boxes.cls, boxes.conf, boxes.xyxy):
